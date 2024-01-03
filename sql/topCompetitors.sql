78ed0e22f9bc2387e2566da481219dcb75afe667
CIHAN GUR <138358253+cihangr@users.noreply.github.com>
2024-01-03 10:46:08 +0200

    topCompetitors Added

---
+++ sql/topCompetitors.sql
@@ -0,0 +1,8 @@
+SELECT H.hacker_id, H.name FROM Hackers H
+JOIN Submissions S ON H.hacker_id = S.hacker_id
+JOIN Challenges C ON S.challenge_id = C.challenge_id
+JOIN Difficulty D ON C.difficulty_level = D.difficulty_level
+WHERE D.score = S.score AND C.difficulty_level = D.difficulty_level
+GROUP BY H.hacker_id, H.name
+HAVING COUNT(S.hacker_id)>1
+ORDER BY COUNT(S.hacker_id) DESC, S.hacker_id ASC;
